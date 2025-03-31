6fd5723170f2f833c8496d23313751ec35b79a19
noob-master-jpb <b8777051595@gmail.com>
2025-03-31 20:51:45 +0000

    sample rag

---
+++ setup.py
@@ -0,0 +1,78 @@
+import os
+import pandas as pd
+from pandas_gbq import to_gbq
+import subprocess
+from google.cloud import bigquery
+from dotenv import load_dotenv
+
+
+load_dotenv()
+project_id = os.getenv("project_id")
+dataset_id = os.getenv("dataset_id")
+table_id = os.getenv("table_id")
+
+dataset_source = os.getenv("dataset_source")
+embeddings_dataset_compress = os.getenv("embeddings_dataset_compress")
+embeddings_dataset = os.getenv("embeddings_dataset")
+
+
+
+print("project id:", project_id)
+print("dataset id:", dataset_id)
+print("table id:", table_id)
+print("dataset source:", dataset_source)
+print("embeddings dataset compress:", embeddings_dataset_compress)
+print("embeddings dataset:", embeddings_dataset)
+
+if embeddings_dataset in os.listdir():
+    print(f"{embeddings_dataset} found")
+else:
+    if embeddings_dataset_compress in os.listdir():
+        print(f"{embeddings_dataset_compress} is in the folder.")
+        print("decompressing to", end=" ")
+        subprocess.run(["tar", "-xvJf", embeddings_dataset_compress], check=True)
+    else:
+        print(f"{embeddings_dataset_compress} is not in the folder.")
+        print("downloading...")
+        subprocess.run(["wget", dataset_source], check=True)
+        print("decompressing to", end=" ")
+        subprocess.run(["tar", "-xvJf", embeddings_dataset_compress], check=True)
+
+print("loading embeddings dataset... to pandas dataframe")
+df = pd.DataFrame(pd.read_pickle(embeddings_dataset))
+df['embedding'] = df['embedding'].apply(list)
+
+print("Connecting to BigQuery...")
+print("Creating BigQuery client...")
+print("Creating dataset...")
+print("Creating table...")
+client = bigquery.Client(project=project_id)
+dataset = bigquery.Dataset(f"{project_id}.{dataset_id}")
+client.create_dataset(dataset, exists_ok=True)
+
+print("Creating Schema...")
+schema = [
+    bigquery.SchemaField("disease", "STRING", mode="REQUIRED"),    
+    bigquery.SchemaField("text", "STRING", mode="REQUIRED"),
+    bigquery.SchemaField("embedding", "FLOAT", mode="REPEATED")
+]
+
+
+table_ref = client.dataset(dataset_id).table(table_id)
+try:
+    client.get_table(table_ref)
+    print("Table already exists. Skipping creation.")
+except:
+    table = bigquery.Table(table_ref, schema=schema)
+    client.create_table(table)
+    print(f"Table {table_id} created in dataset {dataset_id}.")
+    
+chunk_size = 5000  # change based on API limit
+for i in range(0, len(df), chunk_size):
+    chunk = df[i:i + chunk_size]
+    to_gbq(
+        dataframe=chunk,
+        destination_table=f"{dataset_id}.{table_id}",
+        project_id=project_id,
+        if_exists="append" if i > 0 else "replace"
+    )
